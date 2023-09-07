4f2529fe179d52fa8a8fa9c992419a3d8e7ff4cc
kikukawa <2201009@s.asojuku.ac.jp>
2023-09-07 20:56:50 +0900

    画像自体をリンクに変更、システム日付の表示

--- src/components/commonHeader.vue
+++ src/components/commonHeader.vue
@@ -5,33 +5,38 @@
                 <div class="container">
                     <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
                         <div class="d-flex align-items-center my-2 my-lg-0 me-lg-auto text-white text-decoration-none">
-                            <img src="" class="bi me-2" width="160" height="80"
-                                role="img" aria-label="Bootstrap" alt="アイコン">
+                            <div class="date-display text-white">
+                                今日の日付は {{ currentDate }} です
+                            </div>
                         </div>
 
                         <ul class="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small">
                             <li class="Home">
                                 <div class="nav-link text-secondary">
-                                    <img src="@/assets/img/Home.png" class="bi d-block mx-auto mb-1" width="45" height="30">
-                                    <router-link to="/">ホーム</router-link>
+                                    <router-link to="/" style="color: rgb(255, 255, 255);">
+                                    <img src="@/assets/img/Home.png" class="bi d-block mx-auto mb-1" width="45" height="30">ホーム
+                                    </router-link>
                                 </div>
                             </li>
                             <li class="Grave">
                                 <div class="nav-link text-white">
-                                    <img src="@/assets/img/Grave.png" class="bi d-block mx-auto mb-1" width="45" height="30">
-                                    <router-link to="/GravePage">墓</router-link>
+                                    <router-link to="/GravePage" style="color: rgb(255, 255, 255);">
+                                    <img src="@/assets/img/Grave.png" class="bi d-block mx-auto mb-1" width="45" height="30">墓
+                                    </router-link>
                                 </div>
                             </li>
                             <li class="Calendar">
                                 <div class="nav-link text-white">
-                                    <img src="@/assets/img/Calendar.png" class="bi d-block mx-auto mb-1" width="45" height="30">
-                                    <router-link to="CalendarPage">カレンダー</router-link>
+                                    <router-link to="CalendarPage" style="color: rgb(255, 255, 255);">
+                                    <img src="@/assets/img/Calendar.png" class="bi d-block mx-auto mb-1" width="45" height="30">カレンダー
+                                    </router-link>
                                 </div>
                             </li>
                             <li class="Ghost">
                                 <div class="nav-link text-white">
-                                    <img src="@/assets/img/Ghost.png" class="bi d-block mx-auto mb-1" width="45" height="30">
-                                    <router-link to="GhostPage">通知欄</router-link>
+                                    <router-link to="GhostPage" style="color: rgb(255, 255, 255);">
+                                    <img src="@/assets/img/Ghost.png" class="bi d-block mx-auto mb-1" width="45" height="30">通知欄
+                                    </router-link>
                                 </div>
                             </li>
                         </ul>
@@ -41,3 +46,23 @@
         </header>
     </main>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            currentDate: "",
+        };
+    },
+    mounted() {
+        this.getCurrentDate();
+    },
+    methods: {
+        getCurrentDate() {
+            const now = new Date();
+            const options = { year: 'numeric', month: 'long', day: 'numeric' };
+            this.currentDate = now.toLocaleDateString('ja-JP', options);
+        },
+    },
+};
+</script>
